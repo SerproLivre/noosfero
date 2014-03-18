088c33bd2ee07754a9d28fd9ff5e3ebd24808dd7
Evandro Junior <evandrojr@gmail.com>
2014-03-18 20:24:18 +0000

    improved translations

--- plugins/pairwise/lib/pairwise_plugin/pairwise_content.rb
+++ plugins/pairwise/lib/pairwise_plugin/pairwise_content.rb
@@ -8,18 +8,12 @@ class PairwisePlugin::PairwiseContent < Article
   validate_on_create :validate_choices
 
   REASONS_ARRAY = [
-    # {:text => N_("I like both ideas"), :compare => false},
-    # {:text => N_("I think both ideas are the same"), :compare => false},
-    # {:text => N_("I don't know enough about either idea"),:compare => false},
-    # {:text => N_("I don't like either idea"), :compare => false},
-    # {:text => N_("I don't know enough about: "),:compare => true},
-    # {:text => N_("I just can't decide"),:compare => false}
-    {:text => "Eu gostei das duas idéias", :compare => false},
-    {:text => "Eu acho que as duas idéias são iguais", :compare => false},
-    {:text => "Eu não sei o bastante sobre essas idéias",:compare => false},
-    {:text => "Eu não gostei de nenhuma", :compare => false},
-    {:text => "Eu não sei o bastante sobre: ",:compare => true},
-    {:text => "Não consigo me decidir",:compare => false}
+     {:text => N_("I like both ideas"), :compare => false},
+     {:text => N_("I think both ideas are the same"), :compare => false},
+     {:text => N_("I don't know enough about either idea"),:compare => false},
+     {:text => N_("I don't like either idea"), :compare => false},
+     {:text => N_("I don't know enough about: "),:compare => true},
+     {:text => N_("I just can't decide"),:compare => false}
   ]
 
   def initialize(*args)
